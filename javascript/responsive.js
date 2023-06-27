5636f6496d4d8043d9fe17bc056c999f22fa4d72
Sebastian Leañez <133547139+TheSebalex@users.noreply.github.com>
2023-06-27 02:06:21 -0400

    Add files via upload

--- javascript/responsive.js
+++ javascript/responsive.js
@@ -1,15 +1,15 @@
-let menu = document.getElementsByClassName("menu")
-let img = document.getElementById("icono")
-let logo = document.getElementById("logo")
-let deployUl = document.querySelector(".listaResponsive")
-let main = document.querySelector("main")
-let ex = document.getElementById("ex")
+let menu = document.getElementsByClassName("menu");
+let img = document.getElementById("icono");
+let logo = document.getElementById("logo");
+let deployUl = document.querySelector(".listaResponsive");
+let main = document.querySelector("main");
+let ex = document.getElementById("ex");
 
-window.onresize = resize 
-window.onload = resize
-img.onclick = deploy
-main.onclick = nodeploy
-ex.onclick = nodeploy
+window.onresize = resize;
+window.onload = resize;
+img.onclick = deploy;
+main.onclick = nodeploy;
+ex.onclick = nodeploy;
 
 
 function resize(){
@@ -17,21 +17,21 @@ function resize(){
         img.classList.remove("icono");
         for(let i= 0; i < menu.length; i++){
             menu[i].classList.add("esconderOpciones")
-        }
+        };
         logo.classList.add("logoResponsive");       
     } else {
-        img.classList.add("icono")
+        img.classList.add("icono");
         for(let i= 0; i < menu.length; i++){
             menu[i].classList.remove("esconderOpciones")
-        }
+        };
         logo.classList.remove("logoResponsive"); 
     }
-} 
+};
 
 function deploy(){
-    deployUl.classList.add("desplegar")
-}
+    deployUl.classList.add("desplegar");
+};
 
 function nodeploy(){
-    deployUl.classList.remove("desplegar")
+    deployUl.classList.remove("desplegar");
 }
